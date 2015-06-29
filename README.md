9f94027a72ea97b7a70ff528432da6369bc52f3e
Mike <m@thecoded.org>
2015-06-30 00:43:23 +0900

    init commit- challenge solution completed

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+This is a solution for the following challenge.
+To run this code, clone this repo to your computer. Then, in your terminal, run:
+npm install
+Then, run:
+node server.js
 
-http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+The answers will output on your console.
+
+Challenge:
+
+
+Developer Challenge
+The objective of this challenge is to parse a log file and do some analysis on it. It should be an easy task for anyone with previous experience programming. The challenge must be solved in node.js. 
+Sample log can be downloaded here: https://www.dropbox.com/s/22ocrq2eprh7yiu/sample%20%281%29.log?dl=0
+* Required
+Requirements & output
+The log file contains all requests to a server within a specific timeframe. Given the following endpoints:
+
+GET /api/users/{user_id}/count_pending_messages
+GET /api/users/{user_id}/get_messages
+GET /api/users/{user_id}/get_friends_progress
+GET /api/users/{user_id}/get_friends_score
+POST /api/users/{user_id}
+GET /api/users/{user_id}
+
+Where {user_id} is the id of the user calling the backend.
+
+Write a command line program that outputs a small analysis of the sample log containing at least the following:
+
+For each of the URLs above:
+
+- The number of times the URL was called.
+- The mean (average), median and mode of the response time (connect time + service time).
+- The "dyno" that responded the most.
+
+The output should just be simple text lines. Note that we want aggregate data for all users, not stats on specific users.
+What we are looking for
+- Correctness
+- Simple, clear, readable code
+- Memory efficiency (optional - bonus points)
+Questions & Delivery
+If you have any questions to this challenge, please contact us at info@infinitylevels.com
+
+The challenge should be delivered as a link to a public git repository or gist (github.com or bitbucket.com are preferred). 
+
+
+
+Check List
+Your Program:
+  Can be run from the command line
+ Is non-interactive, does not require user input
+ Generates plain text output
+ Returns one entry for each of the 6 endpoints listed above
+
--- server.js
+++ server.js
@@ -1,159 +1,94 @@
 #!/bin/env node
-//  OpenShift sample Node application
+
 var express = require('express');
 var fs      = require('fs');
 
+var cheerio = require("cheerio");
+var http = require('http');
+var stats = require("stats-lite");
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+    var endPoints = [
 
-    //  Scope.
-    var self = this;
+                'count_pending_messages',
+                'get_messages',
+                'get_friends_progress',
+                'get_friends_score',
+                'POST path=/api/users/',
+                'GET path=/api/users/'
 
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+            ]
 
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+    fs.readFile('sample.log', 'utf8', function(err, res1){
+
+                for(j in endPoints){
+                          var allMsg = res1.split('heroku[router]:');
+
+                    var response = [];
+                    var responseTimes = [];
+                    var dynos =[];
+
+                    for(i in allMsg){
+
+                        if(allMsg[i].indexOf(endPoints[j]) != -1 ){
+
+                            
+                            
+                            response.push(allMsg[i]);
+                            dynos.push(allMsg[i].split('dyno=')[1].split(' ')[0])
+                            connectTime = parseFloat(allMsg[i].split('connect=')[1].split('ms')[0]);
+                            serviceTime = parseFloat(allMsg[i].split('service=')[1].split('ms')[0]);
+                            responseTimes.push(connectTime+serviceTime);
+
+                        }
+                    }
+
+                    console.log(endPoints[j]+ " was called " + response.length + " times");
+                    console.log(endPoints[j]+ " mean response time was "+ stats.mean(responseTimes) + "ms");
+                    console.log(endPoints[j]+ " median response time was "+ stats.median(responseTimes) + "ms");
+                    console.log(endPoints[j]+ " mode response time was "+ stats.mode(responseTimes)+ "ms");
+                    
+                    console.log(endPoints[j]+ " most common dynos was " + modeString(dynos));
+
+                }
+
+
+
+            
+
+            })
+
+
+//library function
+
+function modeString(array)
+{
+    if (array.length == 0)
+        return null;
+
+    var modeMap = {},
+        maxEl = array[0],
+        maxCount = 1;
+
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+
+        if (modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;
+
+        if (modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
         }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
+        else if (modeMap[el] == maxCount)
+        {
+            maxEl += '&' + el;
+            maxCount = modeMap[el];
         }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+    }
+    return maxEl;
+}
